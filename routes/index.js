97ba33eb3d55b889beaef9902f7306f096824e11
s4remi <s4remi@gmail.com>
2023-11-16 13:10:17 -0800

    add try and catch ffor getting data

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,19 @@ let router = express.Router();
 
 /* GET home page. */
 router.get("/api/data", async function (req, res) {
-  const datas = await myDB.getDatas();
-  console.log("IN INDEX.JS. Data from MongoDB:", datas);
-  res.json(datas);
+  // Consider validating the input
+  const query = req.query.query || "";
+
+  console.log("/api/data query", query);
+
+  try {
+    const datas = await myDB.getPhotos({ query });
+    console.log("got Data", datas.length);
+    res.status(200).json({ datas });
+  } catch (err) {
+    console.log("error getting Data", err);
+    return res.status(400).json({ error: err });
+  }
 });
 
 export default router;
