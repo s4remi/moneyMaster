8d35dcf270466fa35551760d4f7a8458f8ab01ac
s4remi <s4remi@gmail.com>
2023-11-17 12:14:24 -0800

    add about page

---
+++ front/src/pages/AboutPage.jsx
@@ -0,0 +1,18 @@
+import BasePage from "./BasePage";
+
+export default function AboutPage() {
+  return (
+    <>
+      <BasePage>
+        <h1> About page!</h1>{" "}
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat at
+          quasi similique vero amet nemo et pariatur veritatis tempora
+          repellendus, nesciunt, perferendis aut alias soluta esse, nisi in
+          voluptas nostrum.
+        </div>
+        <h2> here we are</h2>{" "}
+      </BasePage>
+    </>
+  );
+}
