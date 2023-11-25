ef2c84b274036a92edcfbc9803a08e8d2dde8cbe
s4remi <s4remi@gmail.com>
2023-11-25 11:07:00 -0800

    add more structure for about page

--- front/src/pages/AboutPage.jsx
+++ front/src/pages/AboutPage.jsx
@@ -1,17 +1,25 @@
 import BasePage from "./BasePage";
+import AboutImg from "../assets/about.png";
 
 export default function AboutPage() {
   return (
     <>
       <BasePage>
-        <h1> About page!</h1>{" "}
+        <h1> Who we are?</h1>{" "}
         <div>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat at
           quasi similique vero amet nemo et pariatur veritatis tempora
           repellendus, nesciunt, perferendis aut alias soluta esse, nisi in
           voluptas nostrum.
         </div>
-        <h2> here we are</h2>{" "}
+        <h2> Other Production</h2>{" "}
+        <ul>
+          <li>eccomerce book store</li>
+          <li>Protein AR for andriod mobiles</li>
+        </ul>
+        <img src={AboutImg} alt="about png picture" />
+        <h2>Stuck price</h2>
+        <h2>News and Results</h2>
       </BasePage>
     </>
   );
